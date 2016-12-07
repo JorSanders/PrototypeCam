bb4e7c499c2b99ddd35fc9d23c54f556241a83ef
MBesseling <marit_besseling@msn.com>
2016-12-07 09:36:36 +0100

    Eerste poging om bootstrap in de applicatie te krijgen (status: nog niet gelukt)

---
+++ includes/header.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+
+    <head>
+
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+
+        <title> Witchidents </title>
+
+        <!-- Bootstrap -->
+        <link href="/Source Files/library/bootstrap/bootstrap.min.css" rel="stylesheet">
+        <link href="/Source Files/library/bootstrap/font-awesome.min.css" rel="stylesheet">
+        <link href="../style/Style.css" rel="stylesheet">
+
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+     
+         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        
+        <![endif]-->
+    </head>
+
+    <body>
+        <div id="header" class="row"><br>
+
+            header
+        </div>
+        <div id="menu" class="row">
+            <div class="col-md-4">
+                menu left
+            </div>
+            <div class="col-md-4">
+                Logo
+            </div>
+            <div class="col-md-4">
+                Menu right
+            </div>
+        </div>
+        <div id="content">
+
--- view/index.php
+++ view/index.php
@@ -1,27 +1,8 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="/PrototypeCam/style/style.css">
-        <meta charset="UTF-8">
-        <title>Incident melden</title>
-    </head>
-    <body>
-        <div class="Page">
-            <?php
-            require_once("menu.php");
-            ?>
-            <div class="Content">
-                <div id="Centered" >
-                    <form action="" method="POST">
-                        <input class="InputItem" type="text" placeholder="Onderwerp"> <br>
-                        <input class="InputItem" type="text" placeholder="locatie"> <br>
-                        <input class="InputItem" type="text" placeholder="beschrijving"> <br>
-                        <input class="InputItem" type="file" placeholder="upload foto"> <br>
-                        <input class="InputItem" type="text" placeholder="email"> <br>
-                        <input class="InputItem" type="submit">
-                    </form>
-                    
-                </div> 
-            </div>
-    </body>
-</html>
+<?php
+include '../includes/header.php';
+?>
+
+<br>
+<?php
+include '../includes/footer.php';
+?>
