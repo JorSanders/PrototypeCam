3f50affa90ae10b5daae36b454e695a0adb15ea2
LAPTOP\Lennard <Lennardpeerenboom@hotmail.com>
2016-12-14 09:54:29 +0100

    Terug gezet naar oude versie van statistiek pagina die werkt

--- view/Statistics.php
+++ view/Statistics.php
@@ -5,16 +5,6 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
         <link rel="stylesheet" type="text/css" href="/PrototypeCam/style/style.css">
         <meta charset="UTF-8">
-        <?php
-        $StatiscticData = array(
-            array("Sanitair", "20", "#0488A3"),
-            array("Voorraad", "15", "#E3D6BB"),
-            array("Electriciteit", "3", "#EABB1F"),
-            array("Tuinieren", "80", "#236633"),
-            array("Facilitair", "100", "#662C8E"),
-            array("Veiligheid", "20", "#A32822")
-        );
-        ?>
     </head>
 
     <body>
@@ -27,22 +17,11 @@
                     <canvas id="myChart" width="500" height="250"></canvas>
 
                     <script>
-                        
                         var ctx = document.getElementById("myChart");
                         var myChart = new Chart(ctx, {
                             type: 'bar',
                             data: {
-                                labels: [
-                                  <?php
-                                  foreach ($StatiscticData as $Detail) {
-                                      echo " '$detail[0]',";
-                                      
-                                  }
-                                  ?>
-                
-                ],
-                
-//                "Sanitair", "Voorraad", "Electrciteit", "Tuinieren", "Facilitair", "Veiligheid"],
+                                labels: ["Sanitair", "Voorraad", "Electrciteit", "Tuinieren", "Facilitair", "Veiligheid"],
                                 datasets: [{
                                         label: 'Incidenten per categorie',
                                         data: [12, 19, 3, 5, 2, 3],
