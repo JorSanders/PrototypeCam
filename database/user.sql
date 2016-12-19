2d18a9bb3cf1bbc5d6c8e9068daa1d91961042f9
WorkLaptopJor <jorsanders@hotmail.com>
2016-12-19 21:08:12 +0100

    exported database

---
+++ database/user.sql
@@ -0,0 +1,5 @@
+# Rechten voor `Witchidents`@`localhost`
+
+GRANT ALL PRIVILEGES ON *.* TO 'Witchidents'@'localhost' WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES ON `witchidents`.* TO 'Witchidents'@'localhost';
